7b6e1c0e316a117e585050af937e37fa07a2db65
alnever <Teivas@1871>
2021-04-02 17:19:13 +0500

    Unit tests

---
+++ src/test/java/bookstore/SmokeTest.java
@@ -0,0 +1,29 @@
+package bookstore;
+
+import bookstore.controllers.BookController;
+import bookstore.controllers.GenreController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class SmokeTest {
+
+    @Autowired
+    BookController bookController;
+
+    @Autowired
+    GenreController genreController;
+
+    @Test
+    public void bookControllerLoaded() {
+        assertThat(bookController).isNotNull();
+    }
+
+    @Test
+    public void genreControllerLoaded() {
+        assertThat(genreController).isNotNull();
+    }
+}
